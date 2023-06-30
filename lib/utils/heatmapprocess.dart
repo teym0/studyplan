fc33843865d9004487c29cfc74884638e2d808c1
yoontako <129288760+yoontako@users.noreply.github.com>
2023-06-30 20:56:14 +0900

    次回復習日の自動設定を削除、2050年に仮設定 #27

--- lib/utils/heatmapprocess.dart
+++ lib/utils/heatmapprocess.dart
@@ -1,25 +1,6 @@
 import 'package:leadstudy/model/heatcell_model.dart';
 import 'package:leadstudy/model/record_model.dart';
 
-DateTime nextReviewDate(int count, DateTime lastReview) {
-  lastReview = lastReview.copyWith(
-      hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
-  if (count == 0) {
-    return DateTime.now().copyWith(
-        hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
-  } else if (count == 1) {
-    return lastReview.add(const Duration(days: 1));
-  } else if (count == 2) {
-    return lastReview.add(const Duration(days: 7));
-  } else if (count == 3) {
-    return lastReview.add(const Duration(days: 30));
-  } else if (count == 4) {
-    return lastReview.add(const Duration(days: 90));
-  } else {
-    return lastReview.add(const Duration(days: 300));
-  }
-}
-
 List<HeatCellData> generateHeatMapDataFromRecords(
     int bookAmount, List<Record> records) {
   List<HeatCellData> heatMapData = [];
@@ -32,8 +13,7 @@ List<HeatCellData> generateHeatMapDataFromRecords(
     for (var i = 0; i < length; i++) {
       heatMapData[record.start - 1 + i].count += 1;
       heatMapData[record.start - 1 + i].level += 1;
-      heatMapData[record.start - 1 + i].nextReviewAt = nextReviewDate(
-          heatMapData[record.start - 1 + i].count, record.startedAt);
+      heatMapData[record.start - 1 + i].nextReviewAt = DateTime(2050);
     }
   }
   return heatMapData;
